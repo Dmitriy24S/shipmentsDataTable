457b2cabeab226c5034268acd4932b92215b5c75
Dmitriy24S <devilins24@gmail.com>
2023-06-17 08:04:45 +0300

    feat: add Tooltips to Table action buttons

--- src/components/ShipmentsTable/TableItemRow/TableItemRow.tsx
+++ src/components/ShipmentsTable/TableItemRow/TableItemRow.tsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/esm/Button'
 import { IoCloseSharp } from 'react-icons/io5'
 import { TbListDetails } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
+import { UncontrolledTooltip } from 'reactstrap'
 
 import { IShipmentsData } from '../Table'
 
@@ -29,15 +30,22 @@ const TableItemRow = ({ item, handleSelectToDelete }: TableItemRowProps) => {
       <td>{item.consignee}</td>
       <td>
         <ButtonGroup>
+          {/* View details link/button */}
           <Link
             role='button'
             title='View Details'
+            id={`view-${item.orderNo}`}
             className='btn btn-primary px-2 py-1 me-2 rounded'
             to={`/${item.orderNo}`}
           >
             <TbListDetails />
           </Link>
+          <UncontrolledTooltip placement='top' target={`view-${item.orderNo}`}>
+            View Details
+          </UncontrolledTooltip>
+          {/* Delete button */}
           <Button
+            id={`delete-${item.orderNo}`}
             variant='danger'
             className='px-2 py-1 rounded'
             title='Delete Shipment'
@@ -45,6 +53,9 @@ const TableItemRow = ({ item, handleSelectToDelete }: TableItemRowProps) => {
           >
             <IoCloseSharp />
           </Button>
+          <UncontrolledTooltip placement='top' target={`delete-${item.orderNo}`}>
+            Delete Shipment
+          </UncontrolledTooltip>
         </ButtonGroup>
       </td>
     </tr>
